055992ea2b387f145cffbc5540c331913e775881
Peter Son Struschka <me@peter-struschka.com>
2017-09-25 17:50:31 +0700

    tile base class

--- src/main/java/tiles/DirExpTile.java
+++ src/main/java/tiles/DirExpTile.java
@@ -4,4 +4,7 @@ package tiles;
  * Created by joakimnilfjord on 9/25/2017 AD.
  */
 public class DirExpTile extends Tile {
+    public DirExpTile(TileColor color) {
+        super(color);
+    }
 }
--- src/main/java/tiles/SimpleTIle.java
+++ src/main/java/tiles/SimpleTIle.java
@@ -4,4 +4,7 @@ package tiles;
  * Created by joakimnilfjord on 9/25/2017 AD.
  */
 public class SimpleTIle extends  Tile {
+    public SimpleTIle(TileColor color) {
+        super(color);
+    }
 }
