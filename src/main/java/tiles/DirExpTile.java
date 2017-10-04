de1a38afd163174bd6b537cbbb39eb249c2cd799
Peter Son Struschka <me@peter-struschka.com>
2017-10-04 09:52:20 +0700

    added position members to tile

--- src/main/java/tiles/DirExpTile.java
+++ src/main/java/tiles/DirExpTile.java
@@ -4,7 +4,7 @@ package tiles;
  * Created by joakimnilfjord on 9/25/2017 AD.
  */
 public class DirExpTile extends Tile {
-    public DirExpTile(TileColor color) {
-        super(color);
+    public DirExpTile(TileColor color, int xPos, int yPos) {
+        super(color, xPos, yPos);
     }
 }
--- src/main/java/tiles/SimpleTIle.java
+++ src/main/java/tiles/SimpleTIle.java
@@ -4,7 +4,7 @@ package tiles;
  * Created by joakimnilfjord on 9/25/2017 AD.
  */
 public class SimpleTIle extends  Tile {
-    public SimpleTIle(TileColor color) {
-        super(color);
+    public SimpleTIle(TileColor color, int xPos, int yPos) {
+        super(color, xPos, yPos);
     }
 }
