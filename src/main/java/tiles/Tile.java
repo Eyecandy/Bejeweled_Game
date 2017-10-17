a012b3130bdd27ee26808e030a4ff1e6aeec3d52
Peter Son Struschka <me@peter-struschka.com>
2017-10-17 17:57:06 +0700

    Gamelogic from dev

--- src/main/java/tiles/Tile.java
+++ src/main/java/tiles/Tile.java
@@ -3,17 +3,36 @@ package tiles;
 
 abstract public class Tile {
 
-    public enum TileColor {
-        RED, PINK, GREEN, BLUE, PURPLE, YELLOW
-    }
-
     private final TileColor COLOR;
+    private int colorIndex;
 
-    Tile(TileColor color) {
+    public Tile(TileColor color) {
         COLOR = color;
+        if (COLOR==TileColor.BLUE){
+            colorIndex = 0;
+        } else if(COLOR==TileColor.RED){
+            colorIndex = 1;
+        }else if(COLOR==TileColor.GREEN){
+            colorIndex = 2;
+        }else if(COLOR==TileColor.PINK){
+            colorIndex = 3;
+        }else if(COLOR==TileColor.YELLOW){
+            colorIndex = 4;
+        }
     }
 
+    public int getColorIndex(){
+        return colorIndex;
+    }
     public TileColor getCOLOR() {
         return COLOR;
+    }
+
+    public String getColorString(){
+        return COLOR.toString().toLowerCase();
+    }
+
+    public boolean compareColor(Tile tile){
+        return this.getColorString().equals(tile.getColorString());
     }
 }
